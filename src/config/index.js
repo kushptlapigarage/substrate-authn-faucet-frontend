41427cd32ee752e6a2b4eb0f9e1e412b804bfb69
kushptlapigarage <kpatel@apigarage.com>
2020-05-01 13:17:09 -0400

    Adding netflify config

--- src/config/index.js
+++ src/config/index.js
@@ -7,40 +7,18 @@ const dev = {
   POLKASCAN_URI: 'https://polkascan.io/pre/centrifuge-amber-cc2/transaction/'
 };
 const prod = {
-  API_HOST: 'http://localhost:8080',
-  GITHUB_CLIENT_ID: 'bd96258260f769a56370',
-  REDIRECT_URI: 'https://centrifuge-demo.netlify.app/github/callback',
-  POLKASCAN_URI: 'https://polkascan.io/pre/centrifuge-amber-cc2/transaction/'
-};
-const uat = {
-  API_HOST: 'http://localhost:8080',
-  GITHUB_CLIENT_ID: 'a37026ceb8c0357f259f',
-  REDIRECT_URI: 'http://localhost:8000/github/callback',
-  POLKASCAN_URI: 'https://polkascan.io/pre/centrifuge-amber-cc2/transaction/'
-};
-const qa = {
-  API_HOST: 'http://localhost:8080',
-  GITHUB_CLIENT_ID: 'a37026ceb8c0357f259f',
-  REDIRECT_URI: 'http://localhost:8000/github/callback',
-  POLKASCAN_URI: 'https://polkascan.io/pre/centrifuge-amber-cc2/transaction/'
-};
-const staging = {
-  API_HOST: 'http://localhost:8080',
-  GITHUB_CLIENT_ID: 'a37026ceb8c0357f259f',
-  REDIRECT_URI: 'http://localhost:8000/github/callback',
-  POLKASCAN_URI: 'https://polkascan.io/pre/centrifuge-amber-cc2/transaction/'
+  API_HOST: process.env.API_HOST,
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+  REDIRECT_URI: process.env.REDIRECT_URI, // 'https://centrifuge-demo.netlify.app/github/callback',
+  POLKASCAN_URI: process.env.POLKASCAN_URI //'https://polkascan.io/pre/centrifuge-amber-cc2/transaction/'
 };
 
 const config =
   currentEnv === 'production'
     ? prod
-    : currentEnv === 'development'
-      ? dev
-      : currentEnv === 'uat'
-        ? uat
-        : currentEnv === 'qa'
-          ? qa
-          : staging;
+    : dev;
+
+console.log('The config : ', config);
 
 const baseOptions = {
   url: config.API_HOST,
